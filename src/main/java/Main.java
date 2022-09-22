73ab6ce153fbfe45b9bb1fb89b20b9ec3871c48f
Gursharanpreet Singh <gp3.singh@gmail.com>
2022-09-22 18:50:21 +0530

    Stage 4: Handle concurrent clients

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,53 @@ import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
 
+class ClientConnectionHandler implements Runnable {
+
+	private final Socket clientSocket;
+
+	ClientConnectionHandler(Socket clientSocket) {
+		this.clientSocket = clientSocket;
+	}
+
+	public void run() {
+
+		BufferedReader clientRequestReader = null;
+		OutputStreamWriter clientResponseWriter = null;
+
+		try {
+			// Prepare to read data sent by client
+			clientRequestReader = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream(), StandardCharsets.UTF_8));
+
+			// Prepare to send data back to client
+			clientResponseWriter = new OutputStreamWriter(this.clientSocket.getOutputStream(), StandardCharsets.UTF_8);
+
+			while( true ) {
+				// Read the data sent by client
+				String line = clientRequestReader.readLine();
+
+				// Send response back to client
+				clientResponseWriter.write("+PONG\r\n");
+				clientResponseWriter.flush();
+			}
+		} catch (IOException e) {
+			System.out.println("IOException: " + e.getMessage());
+		} finally {
+			try {
+				// Gracefully close stream reader/writer
+				if (clientRequestReader != null) {
+					clientRequestReader.close();
+				}
+				if (clientResponseWriter != null) {
+					clientResponseWriter.close();
+				}
+			} catch (IOException e) {
+				System.out.println("IOException: " + e.getMessage());
+			}
+		}
+	}
+}
+
+
 public class Main {
 
 	private static int REDIS_PORT = 6379;
@@ -17,32 +64,20 @@ public class Main {
 
 		ServerSocket serverSocket = null;
 		Socket clientSocket = null;
-		BufferedReader clientRequestReader = null;
-		OutputStreamWriter clientResponseWriter = null;
 
 		try {
 			// Create a socket
 			serverSocket = new ServerSocket(REDIS_PORT);
 			serverSocket.setReuseAddress(true);
 
-			// Wait for connection from client.
-			clientSocket = serverSocket.accept();
-
-			// Prepare to read data sent by client
-			clientRequestReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(), StandardCharsets.UTF_8));
-
-			// Prepare to send data back to client
-			clientResponseWriter = new OutputStreamWriter(clientSocket.getOutputStream(), StandardCharsets.UTF_8);
-
 			while( true ) {
-				// Read the data sent by client
-				String line = clientRequestReader.readLine();
+				// Wait for connection from a new client
+				clientSocket = serverSocket.accept();
 
-				// Send response back to client
-				clientResponseWriter.write("+PONG\r\n");
-				clientResponseWriter.flush();
+				// Use a new thread to handle connection with this client
+				Thread newConnectionHandler = new Thread(new ClientConnectionHandler(clientSocket));
+				newConnectionHandler.start();
 			}
-
 		} catch (IOException e) {
 			System.out.println("IOException: " + e.getMessage());
 		} finally {
@@ -50,12 +85,6 @@ public class Main {
 				// Gracefully close client socket
 				if (clientSocket != null) {
 					clientSocket.close();
-			   	}
-				if (clientRequestReader != null) {
-					clientRequestReader.close();
-				}
-				if (clientResponseWriter != null) {
-					clientResponseWriter.close();
 				}
 			} catch (IOException e) {
 				System.out.println("IOException: " + e.getMessage());
